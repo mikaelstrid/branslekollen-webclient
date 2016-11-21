589beab60df1e187df96adf2fb5f1850bc17a70e
Mikael Strid <mikael.strid@outlook.com>
2016-11-21 12:12:52 +0100

    Färdigställde "lägg till fordon".

--- src/branslekollen-webclient/wwwroot/app/components/create-vehicle/create-vehicle.component.js
+++ src/branslekollen-webclient/wwwroot/app/components/create-vehicle/create-vehicle.component.js
@@ -2,17 +2,29 @@
     .component('createVehicle',
     {
         templateUrl: 'app/components/create-vehicle/create-vehicle.template.html',
-        controller: ['vehicleService',
-            function CreateVehicleController(vehicleService) {
-                this.vehicleName = '';
-                this.fuel = 'petrol';
+        controller: ['$location', 'vehicleService',
+            function CreateVehicleController($location, vehicleService) {
+                var self = this;
+                self.inProgress = false;
+                self.createOk = false;
+                self.createFailed = false;
+                self.vehicleName = '';
+                self.fuel = 'petrol';
 
-                this.submit = function () {
+                self.submit = function () {
+                    self.inProgress = true;
+                    self.createOk = false;
+                    self.createFailed = false;
                     vehicleService.create(this.vehicleName, this.fuel)
                         .then(
-                            function(response) { alert('OK!'); },
-                            function(error) { alert('Det gick inte att lägga till fordonet. :('); }
-                        );
+                            function(response) {
+                                $location.path('/dashboard');
+                            },
+                            function(error) {
+                                self.createFailed = true;
+                            }
+                        )
+                        .finally(function() { self.inProgress = false; });
                 }
             }
         ]
