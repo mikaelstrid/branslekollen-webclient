51a4c8c7e6da339ef55e13701d5512d081d4fa74
Mikael Strid <mikael.strid@outlook.com>
2016-11-22 12:43:20 +0100

    Implemterade en enkel historik-vy.

--- src/branslekollen-webclient/wwwroot/app/components/history/history.component.js
+++ src/branslekollen-webclient/wwwroot/app/components/history/history.component.js
@@ -2,8 +2,37 @@
     .component('history',
     {
         templateUrl: 'app/components/history/history.template.html',
-        controller: function HistoryController() {
+        controller: [
+            '$log', 'vehicleService',
+            function HistoryController($log, vehicleService) {
+                var self = this;
+                self.loading = false;
+                self.noConnection = false;
+                self.vehicles = [];
+                self.selectedVehicle = null;
 
-        }
+                self.loadVehicles = function() {
+                    self.loading = true;
+                    self.addRefuelingFailed = false;
+                    vehicleService.getAll()
+                        .then(
+                            function(response) {
+                                self.noConnection = false;
+                                if (response.data && response.data.length > 0) {
+                                    self.vehicles = response.data;
+                                    self.selectedVehicle = self.vehicles[0];
+                                }
+                            },
+                            function(error) {
+                                $log.log('Error getting vehicles, error: ' + JSON.stringify(error));
+                                self.noConnection = true;
+                            }
+                        )
+                        .finally(function() { self.loading = false; });
+                }
+
+                self.loadVehicles();
+            }
+        ]
     });
 
---
+++ src/branslekollen-webclient/wwwroot/app/directives/bk-loading-vehicles.directive.js
@@ -0,0 +1,11 @@
+﻿angular.module('myApp')
+.directive('bkLoadingVehicles', function () {
+    return {
+        template: 
+            '<div class="row">' +
+                '<div class="medium-12 columns">' + 
+                    '<p>Laddar fordon...</p>' +
+                '</div>' + 
+            '</div>'
+    };
+});
